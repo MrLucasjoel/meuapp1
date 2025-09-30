3a04fcd345ab3d1076de8b28058e8f721ad815c7
LUCAS JOEL PFLEGER <lucas_j_pfleger@estudante.sesisenai.org.br>
2025-09-30 17:10:07 -0300

    segunda alteração

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# meuapp
+# meuapp1
 
 A new Flutter project.
---
+++ lib/src/app/meuApp.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Estrutura de um app Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World Devs!'),
+        ),
+      ),
+    );
+  }
+}
+
+class TelaAppBar extends StatelessWidget {
+  const TelaAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'DEVS2BLU First App',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Text(
+          'E ai galera!!',
+          style: TextStyle(fontSize: 20, color: Colors.blue[900]),
+        ),
+      ),
+    );
+  }
+}
